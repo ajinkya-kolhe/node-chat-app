6ca33950af20587c18d93fca4f1f4153fa45bb39
Ajinkya Kolhe <ajinkya.k15@gmail.com>
2017-07-06 20:39:45 +1000

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -18,7 +18,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-//Emit event to single connection
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app.',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined.',
+    createdAt: new Date().getTime()
+  });
+
+//socket Emit event to single connection
 
 socket.on('createMessage', (message) => {
   console.log('createMessage', message);
@@ -28,6 +40,12 @@ socket.on('createMessage', (message) => {
     text: message.text,
     createdAt: new Date().getTime()
   });
+//socket broadcast emits evetn to all connection except current one
+  // socket.broadcast.emit('newMessage', {
+  //    from: message.from,
+  //    text: message.text,
+  //    createdAt: new Date().getTime()
+  //  });
 });
 
 //Listen to Event
